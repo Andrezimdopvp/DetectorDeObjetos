57fde66a20c1df0ad01a4fcf5582c372938edd56
Andrezimdopvp <102673694+Andrezimdopvp@users.noreply.github.com>
2023-10-06 19:58:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function quarto(){
+    window.location="./screens/quarto.html"
+}
+function sala(){
+    window.location="./screens/sala.html"
+}
+function varanda(){
+    window.location="./screens/varanda.html"
+}
+function jardim(){
+    window.location="./screens/jardim.html"
+}
+function banheiro(){
+    window.location="./screens/banheiro.html"
+}
